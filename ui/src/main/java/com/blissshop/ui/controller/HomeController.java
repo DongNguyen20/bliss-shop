e6a49da8e16d5e61409b0d167b2beaaa919843b9
dong.nguyen <dong.nguyen@cherry-solutions.com>
2024-10-16 00:21:07 +0700

    ui: delete hard item in html

--- ui/src/main/java/com/blissshop/ui/controller/HomeController.java
+++ ui/src/main/java/com/blissshop/ui/controller/HomeController.java
@@ -1,16 +1,34 @@
 package com.blissshop.ui.controller;
 
+import com.blissshop.ui.model.Category;
+import com.blissshop.ui.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to the UI Home Page!");
-        model.addAttribute("loggedIn", false);
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Computer", "Description", "/img/category/computer.gif"));
+        categories.add(new Category("Pizza", "Description", "/img/category/pizza.gif"));
+        categories.add(new Category("Going", "Description", "/img/category/go.gif"));
+        categories.add(new Category("Machine", "Description", "/img/category/machine.gif"));
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Product 1", "Description 1", 20.00, "/img/items/item.jpg"));
+        products.add(new Product("Product 2", "Description 2", 30.00, "/img/items/item1.jpg"));
+        products.add(new Product("Product 3", "Description 3", 40.00, "/img/items/item2.jpg"));
+        products.add(new Product("Product 4", "Description 4", 30.00, "/img/category/go.gif"));
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("products", products);
+        model.addAttribute("loggedIn", true);
         return "home";
     }
 
