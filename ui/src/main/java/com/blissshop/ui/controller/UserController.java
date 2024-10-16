d3e4086ca91a83f3f5a10948140130b1ec7bb946
dong.nguyen <dong.nguyen@cherry-solutions.com>
2024-10-16 23:49:25 +0700

    ui: fix if call api failed

--- ui/src/main/java/com/blissshop/ui/controller/UserController.java
+++ ui/src/main/java/com/blissshop/ui/controller/UserController.java
@@ -4,7 +4,6 @@ import com.blissshop.ui.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/users")
@@ -26,18 +26,20 @@ public class UserController {
     public String goUserManagement(Model model) {
         // using load balancer
         String url = "lb://user-service/api/users";
-        ResponseEntity<List<User>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {}
-        );
-
-        if (response.getStatusCode() == HttpStatus.OK) {
-            model.addAttribute("users", response.getBody());
-        } else {
-            System.out.println("Failed to retrieve users: " + response.getStatusCode());
+        List<User> users;
+        try {
+            ResponseEntity<List<User>> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<>() {
+                    }
+            );
+            users = response.getBody();
+        } catch (Exception e) {
+            users = new ArrayList<>();
         }
+        model.addAttribute("users", users);
         return "user_management";
     }
 }
